d279f33b1f67a6e9491a75a1620737010f874822
Iván Juárez Núñez <ivan@blindgazer.com>
2020-07-02 00:05:09 -0500

    feat: simple shader

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,14 +1,12 @@
-precision highp float;
+precision mediump float;
 
 uniform float u_time;
+uniform vec2 u_mouse;
 varying vec2 v_uv;
 
 void main() {
-  gl_FragColor = vec4(
-    vec3(
-      (sin(u_time * v_uv.x) + 1.0) / 2.0,
-      (cos(u_time * v_uv.y) + 1.0) / 2.0,
-      (cos(u_time) + 1.0) / 2.0
-    ),
-    1.0);
+  vec3 color = vec3(v_uv, 0.25 + 0.5 * sin(u_time));
+  float hole = sin(distance(v_uv, u_mouse) * 12.);
+  color = mix(color, vec3(hole), -0.5);
+  gl_FragColor = vec4(color, 1.0);
 }

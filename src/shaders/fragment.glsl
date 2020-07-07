ff86d8105dbcd33d176367d28fbd5c612ad6c364
Iván Juárez Núñez <ivan@blindgazer.com>
2020-07-07 16:30:05 -0500

    feat: shader working

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -7,16 +7,32 @@ precision mediump float;
 uniform float u_time;
 uniform vec2 u_mouse;
 uniform vec2 u_ratio;
-uniform sampler2D u_texture;
-uniform vec2 u_resolution;
+uniform sampler2D u_tImage;
+uniform sampler2D u_tNoise;
+
+// https://www.shadertoy.com/view/MdlXz8
+// https://www.shadertoy.com/view/4t33z8
 varying vec2 v_uv;
+varying vec2 v_uv2;
 
 void main() {
+
   vec2 uv = v_uv;
-  // vec2 mouse = u_mouse * u_ratio;
-  // vec3 color = vec3(uv, 0.25 + 0.5 * sin(u_time));
-  // float hole = sin(distance(uv, mouse) * 12.);
-  // color = mix(color, vec3(hole), -0.5);
-  vec4 color = texture2D(u_texture, uv);
-  gl_FragColor = color;
+
+  vec2 warpUV = 2. * uv;
+	float d = length( warpUV );
+	vec2 st = warpUV * 0.1 + 0.2 * vec2(
+    cos( 0.071 * u_time * 2. + d ),
+		sin( 0.073 * u_time * 2. - d )
+  );
+  vec3 warpedCol = texture2D( u_tNoise, st ).xyz * 2.0;
+  float w = max( warpedCol.r, 0.85);
+  vec2 offset = 0.01 * cos( warpedCol.rg * 3.14159 );
+  vec3 col = texture2D( u_tImage, uv + offset ).rgb * vec3(1.0, 1.0, 1.5) ;
+  col *= w * 2.0;
+
+  //float force = distance(u_mouse * u_ratio, v_uv2 * u_ratio);
+
+  //vec3 color = //texture2D(u_tImage, v_uv + force * 0.1).rgb;
+  gl_FragColor = vec4(mix(col, texture2D(u_tImage, uv + offset).rgb, 0.8), 1.0);
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,8 +1,15 @@
 attribute vec2 position;
+uniform vec2 u_scale;
+uniform vec2 u_offset;
+uniform vec2 u_origin;
+
 attribute vec2 uv;
 varying vec2 v_uv;
+varying vec2 v_uv2;
 
 void main() {
-  v_uv = uv;
+  v_uv = (uv - u_origin) / u_scale + u_offset;
+  v_uv += u_origin;
+  v_uv2 = uv;
   gl_Position = vec4(position, 0.0, 1.0);
 }
